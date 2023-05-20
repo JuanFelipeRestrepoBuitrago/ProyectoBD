08e210b4e0299c278fa00d7446011758f8e47bc3
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-05-20 15:31:08 -0500

    Delete Unnecessary
    Some unnecessary files were deleted, they were in the static directory

--- MySQL/interfaces/admin.py
+++ MySQL/interfaces/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Estudiantes)
+admin.site.register(Clases)
+admin.site.register(Materias)
+admin.site.register(Profesores)
+admin.site.register(Administradores)
+admin.site.register(Aulas)
+admin.site.register(Facturas)
+admin.site.register(MateriasAprobadas)
+admin.site.register(MateriasPrerrequisito)
+admin.site.register(Registros)

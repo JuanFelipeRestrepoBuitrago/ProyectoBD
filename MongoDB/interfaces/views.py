2df0dad37379e3f3a761b1ad7b7ee1e50c9736af
julianvb03 <valenciajuliann@hotmail.com>
2023-05-24 22:35:50 -0500

    comit

--- MongoDB/interfaces/views.py
+++ MongoDB/interfaces/views.py
@@ -110,5 +110,21 @@ def home_estudiante(request, documento):
 
 #Ver las clases
 def get_clases(request,documento):
+    estudiante = Estudiantes.objects.select_for_update().get(documento_identidad=documento)
+    '''registros = Registros.objects.filter(codigo_estudiante=estudiante.id, fecha_registro__year=datetime.now().year)
+    clases = Clases.objects.filter(id_clase__in=registros.values('id_clase'))'''
+    registros = Registros.objects.filter(codigo_estudiante=estudiante.id, fecha_registro__year=datetime.now().year)
+    clases = Clases.objects.filter(id__in=registros.values('id_clase_id'))
     if request.method == 'GET':
-        
+        return render(request, 'Estudiante/clases.html', {
+            'title': f'Clases {estudiante.nombre_completo}',
+            'estudiante': estudiante,
+            'clases': clases,
+        })
+    else:
+        clase = request.POST['id_clase']
+        registro = registros.get(id_clase=clase)
+
+        registro.delete()
+        messages.success(request, 'Materia eliminada con éxito')
+        return redirect('clases_estudiante', documento=estudiante.documento_identidad)
